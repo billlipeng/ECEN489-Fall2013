d42bbfedbbe4ea1f77dfd2314fe50a2d8232c18e
r_narayanan_92 <r_narayanan_92@bd20e25c-6709-46e7-9c33-37c0a025eed4>
2013-08-30 19:40:47 +0000

    This is the complete project for the first task (addition of two numbers in C++).

---
+++ r_narayanan_92/Cpp/Code1/Code1.cpp
@@ -0,0 +1,21 @@
+/*
+ * Code1.cpp
+ *
+ *  Created on: Aug 28, 2013
+ *      Author: Narayanan Rengaswamy
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	std::cout << "Enter two numbers:" << std::endl;
+	int v1 = 0, v2 = 0;
+	std::cin >> v1 >> v2;
+	std::cout << "The sum of " << v1 << " and " << v2
+	<< " is " << v1 + v2 << std::endl;
+
+}
+
