495ce79a511fdfd5eaa4484e2603871f2e7dc259
zpartal <zpartal@bd20e25c-6709-46e7-9c33-37c0a025eed4>
2013-09-01 01:15:40 +0000

    Assignment 1 completion

---
+++ zpartal/Cpp/Code1/main.cpp
@@ -0,0 +1,22 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Aug 31, 2013
+ *      Author: Zachary
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	cout << "Enter two numbers seperarated by a space and press enter";
+	int a = 0;
+	int b = 0;
+	cin >> a >> b;
+
+	cout << "The sum of " << a << " and " << b << " is " << a + b;
+
+	return 0;
+}
