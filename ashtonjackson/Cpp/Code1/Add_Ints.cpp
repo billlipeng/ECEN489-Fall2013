aea77d98f71820b7d2469fc8a000fdd5ee02f49b
ashtonjackson <ashtonjackson@bd20e25c-6709-46e7-9c33-37c0a025eed4>
2013-09-02 22:50:24 +0000

    Assignment 1

---
+++ ashtonjackson/Cpp/Code1/Add_Ints.cpp
@@ -0,0 +1,19 @@
+/*
+ * Add_Ints.cpp
+ *
+ *  Created on: Aug 30, 2013
+ *      Author: ashton
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num1, num2, sum;
+	cout << "Enter two integers:" << endl;
+	cin >> num1 >> num2;
+	sum = num1 + num2;
+	cout << "The sum is " << sum << "." <<endl;
+	return 0;
+}
