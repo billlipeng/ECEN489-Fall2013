359b1c74f47c5694e6df21d8cfdc08f21e1c73ab
b_bass <b_bass@bd20e25c-6709-46e7-9c33-37c0a025eed4>
2013-09-02 22:44:00 +0000

    Share project file "Assn1" into "https://lemur.tamu.edu/svn/ECEN489-Fall2013".

---
+++ b_bass/Cpp/Code1/src/Assn1.cpp
@@ -0,0 +1,23 @@
+//============================================================================
+// Name        : Assn1.cpp
+// Author      : Brian Bass
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	cout << "Please Enter two numbers" << endl;
+
+	double x1 = 0.0, x2 = 0.0;
+
+	cin >> x1 >> x2;
+
+	cout << "The sum of your two numbers is " << x1 + x2 << endl;
+
+	return 0;
+}
