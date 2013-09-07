276c2a62dc57d3b55f37905ca8e161ed9fedfad7
clayton.crawford <clayton.crawford@bd20e25c-6709-46e7-9c33-37c0a025eed4>
2013-09-07 01:47:14 +0000

    Lab 2 Commit

---
+++ clayton.crawford/Arduino/Lab2/Lab2.ino
@@ -0,0 +1,24 @@
+int photoSensor = 0;
+
+void setup()
+{
+  pinMode(2, OUTPUT); 
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  // read in voltage divider data
+  int dividerSensor = analogRead(A0);
+  // convert data into a voltage in decimal notation  
+  float dividerVoltage = dividerSensor * (5.0 / 1024.0);
+  // output to the serial monitor this voltage
+  Serial.print("\nVoltage @ photoresistor divider: ");    
+  Serial.print(dividerVoltage);
+  
+  // if the photoresistor is covered, the swithcing circuit turns on
+  photoSensor = analogRead(A0);
+  if(photoSensor < 400)
+   digitalWrite(2, HIGH); 
+  else digitalWrite(2, LOW); 
+}
