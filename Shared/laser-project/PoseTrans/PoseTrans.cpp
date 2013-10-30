8b26719027a27fd6f1e1b1cc0997896249177e67
clayton.crawford <clayton.crawford@bd20e25c-6709-46e7-9c33-37c0a025eed4>
2013-10-30 18:58:50 +0000

    Updated PoseTrans.cpp to read and parse JSON data from an input file to simulate serial reading. Also added was creating matrices otu of JSON data from an input file, performing matrix multiplication, creating a JSON object out of the product matrix, and outputting the JSON product matrices to a file.

--- Shared/laser-project/PoseTrans/PoseTrans.cpp
+++ Shared/laser-project/PoseTrans/PoseTrans.cpp
@@ -60,34 +60,6 @@ void readSerialPort() {
 
 int main () {
 
-	// testing with Armadillo matrices
-	cout << "\nMatrix Multiplication Test!\n";
-	mat m1(4,4); // 4x4 matrix
-	mat m2(4,1); // 4x1 matrix
-	mat result(4,1); // 4x1 matrix 
-
-	// fill matrix m1 with random info except last row is 0 0 0 1
-	for (int i = 0; i < 4; i++) {
-		for (int j = 0; j < 4; j++) {
-			if (i != 3)
-	  			m1(i, j) =  i * 10;
-	  		else if (i == 3 && j != 3)
-	  			m1(i, j) = 0;
-	  		else 
-	  			m1(i, j) = 1;
-		}
-	}
-
-	// fill matrix m2 with all 2's
-  	for (int i = 0; i < 4; ++i) {
-  		m2(i, 0) = 2;
-  	}
-
-  	result = m1 * m2;
-  	cout << "M1 [4x4]\n" << m1 << endl;
-  	cout << "M2 [4x1]\n" << m2 << endl;
-  	cout << "Result [4x1]\n" << result << endl;
-
   	// generate sample JSON data file (emulate serial data from microcontroller)
   	Value poseTransInput;
 
@@ -105,8 +77,11 @@ int main () {
   		poseTransInput["r1"] = rand() % 10;
   		poseTransInput["r2"] = rand() % 10;
   		poseTransInput["r3"] = rand() % 10;
+  		poseTransInput["l1"] = rand() % 10;
+  		poseTransInput["l2"] = rand() % 10;
+  		poseTransInput["l3"] = rand() % 10;
 
-  		FastWriter fw;
+  		StyledWriter fw;
   		string poseInputJSON = fw.write(poseTransInput);
 
   		FILE *f = fopen("PoseTransInput", "a");
@@ -118,31 +93,114 @@ int main () {
   		fclose(f);
   	}
 
+  	/*	The following code is testing reading JSON data from an input file,
+	*	creating JSON objects from that data, and then outputting the new
+	*	JSON objects to a separate file.		
+	*/
   	Value PoseJSONobj;
   	Reader reader;
-  	ifstream ifs("PostTransInput", ifstream::in);
+  	ifstream ifs;
+  	ifs.open("PoseTransInput", ios::in);
   	vector<Value> JSONobjsVector;
   	string JSONobj;
   	char test;
-  	int counter = 0;
-  	while (!ifs.eof()) {
-  		cout << counter++ << endl;
-  		ifs >> test;
-  		if (test == '}') {
+  	while (ifs.is_open()) {
+  		ifs.get(test);
+  		if (ifs.eof()){
+  			ifs.close();
+  			break;
+  		} else if (test == '}') {
+  			JSONobj += test;
   			bool parsingSuccessful = reader.parse(JSONobj, PoseJSONobj, false);
   			if (!parsingSuccessful) {
   				cout << reader.getFormatedErrorMessages();
   			} else JSONobjsVector.push_back(PoseJSONobj);
+  			PoseJSONobj.clear();
+  			JSONobj.clear();
   		} else if (test != '}') {
   			JSONobj += test;
   		} else { cout << "\nError parsing!\n"; exit(1);}
-
   	}
   	
+  	FILE *out = fopen("PoseTransReadTest", "w");
+	if (out == NULL) {
+		printf("\nError opening file!\n");
+		exit(1);
+	}
+
   	StyledWriter sw;
   	for (int i = 0; i < JSONobjsVector.size(); ++i) {
   		cout << sw.write(JSONobjsVector[i]);
+  		fprintf(out, "%s", sw.write(JSONobjsVector[i]).c_str());
   	}
+  	fclose(out);
+
+
+  	/*	The following code is testing the creation of matrices 
+  	*	using the data read from an input file and stored in the 
+  	*	the vector JSONobjsVector. Then it will test matrix multiplication,
+  	*	creating JSON objects from the products, and writing those objects to a fle.
+  	*/  
+  	mat pose(4,4);
+  	mat vec(4,1);
+  	mat newPose(4,1);
+
+  	FILE *o = fopen("PoseTransOutput", "w");
+	if (o == NULL) {
+		printf("\nError opening file!\n");
+		exit(1);
+	}
+
+  	for (int i = 0; i < JSONobjsVector.size(); ++i) {
+  		// create 4x4 pose matrix
+  		cout << "\nCreating 4x4 pose matrix...\n";
+  		// column 0 is x'
+  		pose(0,0) = JSONobjsVector[i].get("x\'1", 0).asInt();
+  		pose(1,0) = JSONobjsVector[i].get("x\'2", 0).asInt();
+  		pose(2,0) = JSONobjsVector[i].get("x\'3", 0).asInt();
+  		pose(3,0) = 0;
+
+  		// column 1 is y'
+  		pose(0,1) = JSONobjsVector[i].get("y\'1", 0).asInt();
+  		pose(1,1) = JSONobjsVector[i].get("y\'2", 0).asInt();
+  		pose(2,1) = JSONobjsVector[i].get("y\'3", 0).asInt();
+  		pose(3,1) = 0;
+
+  		// column 2 is z'
+  		pose(0,2) = JSONobjsVector[i].get("z\'1", 0).asInt();
+  		pose(1,2) = JSONobjsVector[i].get("z\'2", 0).asInt();
+  		pose(2,2) = JSONobjsVector[i].get("z\'3", 0).asInt();
+  		pose(3,2) = 0;
+
+  		// column 3 is r vector
+  		pose(0,3) = JSONobjsVector[i].get("r1", 0).asInt();
+  		pose(1,3) = JSONobjsVector[i].get("r2", 0).asInt();
+  		pose(2,3) = JSONobjsVector[i].get("r3", 0).asInt();
+  		pose(3,3) = 1;
+
+  		cout << "Creating 4x1 vector matrix...\n";
+  		// create 4x1 vector matrix
+  		vec(0,0) = JSONobjsVector[i].get("l1", 0).asInt();
+  		vec(1,0) = JSONobjsVector[i].get("l2", 0).asInt();
+  		vec(2,0) = JSONobjsVector[i].get("l3", 0).asInt();
+  		vec(3,0) = 1;
+
+  		newPose = pose * vec;
+
+  		// begin creating JSON object from product
+  		cout << "\nCreating JSON Object from matrix product...\n";
+  		Value newPoseJSON;
+  		newPoseJSON["x"] = newPose(0,0);
+  		newPoseJSON["y"] = newPose(1,0);
+  		newPoseJSON["z"] = newPose(2,0);
+  		newPoseJSON["constant"] = newPose(3,0);
+
+  		// output JSON data to file
+	  	StyledWriter sw;
+	  	fprintf(o, "%s", sw.write(newPoseJSON).c_str());
+	} 
+	fclose(o);
+
 
   	return 0;
 }
